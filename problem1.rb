c97d6422917e617b65728e6ed1ab7a494f95cb3c
Kylan Plunk <kaplunk1@memphis.edu>
2024-03-15 03:09:43 -0500

    task

--- problem1.rb
+++ problem1.rb
@@ -1,3 +1,8 @@
+# Task: Create a simple text-based DSL for generating HTML. Implement a method
+# tag that takes the name of an HTML tag and its content as arguments. The method
+# should return a string representation of the HTML tag. Use this method to generate
+# a paragraph and a header tag without using parentheses.
+
 def tag(name, content)
   return "<#{name}>#{content}</#{name}>"
 end
