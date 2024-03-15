297f5bd7ec619a7e5d8bceb0f08926b4787e58e8
Kylan Plunk <kaplunk1@memphis.edu>
2024-03-15 03:43:45 -0500

    problem6

---
+++ problem6.rb
@@ -0,0 +1,7 @@
+# Task: Create a hash named student with symbols as keys and appropriate values
+# for :name, :age, and :grade. Print the value of each key in a sentence.
+student = {name: "Alice", age: 20, grade: "A"}
+
+puts "Name: #{student[:name]}"
+puts "Age: #{student[:age]}"
+puts "Grade: #{student[:grade]}"
