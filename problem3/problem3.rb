a2dd3f8ee8e2bfecc9636967607e356ec1ed16c0
Kylan Plunk <kaplunk1@memphis.edu>
2024-03-15 03:17:45 -0500

    forgor the begin

--- problem3/problem3.rb
+++ problem3/problem3.rb
@@ -3,8 +3,10 @@
 # print a custom error message. Test your method with both valid and zero divisor
 # cases.
 def safe_divide(a, b)
+  begin
     result = a / b
     return result
+  end
   rescue ZeroDivisionError
     puts "Error: Division by zero is not allowed."
 end
