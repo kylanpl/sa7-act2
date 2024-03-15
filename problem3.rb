7365c341de8c00c63835c44795586195648a5cf8
Kylan Plunk <kaplunk1@memphis.edu>
2024-03-15 03:09:58 -0500

    problem3

---
+++ problem3.rb
@@ -0,0 +1,13 @@
+# Task: Write a method safe_divide that accepts two numbers as parameters and
+# returns their division. Use exception handling to catch division by zero errors and
+# print a custom error message. Test your method with both valid and zero divisor
+# cases.
+def safe_divide(a, b)
+    result = a / b
+    return result
+  rescue ZeroDivisionError
+    puts "Error: Division by zero is not allowed."
+end
+
+
+puts safe_divide(10, 3)
